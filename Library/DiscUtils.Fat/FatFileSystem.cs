2a317179092b3b9eb3ac74fceabcb4bcac9b2e18
Daniele Debernardi <drebrez@gmail.com>
2018-01-11 18:27:37 +0100

    Support reading FAT long file names (VFAT)

--- Library/DiscUtils.Fat/FatFileSystem.cs
+++ Library/DiscUtils.Fat/FatFileSystem.cs
@@ -840,7 +840,7 @@ namespace DiscUtils.Fat
                 FileName name;
                 try
                 {
-                    name = new FileName(pathElements[i], FatOptions.FileNameEncoding);
+                    name = new FileName(pathElements[i], FatOptions.FileNameEncoding, false);
                 }
                 catch (ArgumentException ae)
                 {
@@ -1607,7 +1607,7 @@ namespace DiscUtils.Fat
                 parent = null;
                 return 0;
             }
-            entryId = dir.FindEntry(new FileName(pathEntries[pathOffset], FatOptions.FileNameEncoding));
+            entryId = dir.FindEntry(new FileName(pathEntries[pathOffset], FatOptions.FileNameEncoding, true));
             if (entryId >= 0)
             {
                 if (pathOffset == pathEntries.Length - 1)
